9d32a4596a129a0a283956eb35b7c946bc855070
ISHII Tsuubito <ishii@buffer-renaiss.com>
2015-04-01 07:36:29 +0900

    Initial Commit

---
+++ F2bitSequence/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  F2bitSequence
+//
+//  Created by ISHII 2bit on 2015/04/01.
+//  Copyright (c) 2015年 bufferRenaiss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
